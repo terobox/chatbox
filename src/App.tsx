8a9a6881b7c58c652ad97e9f9887afc3bc02afbc
terobox <changnianyong@gmial.com>
2023-07-18 15:45:12 +0800

    0

--- src/App.tsx
+++ src/App.tsx
@@ -470,12 +470,7 @@ function Main() {
                                     </IconButton>
                                 </ListItemIcon>
                                 <ListItemText>
-                                    <Badge color="primary" variant="dot" invisible={!store.needCheckUpdate}
-                                    sx={{ paddingRight: '8px' }} >
-                                        <Typography sx={{ opacity: 0.5 }}>
-                                            {t('About')} ({store.version})
-                                        </Typography>
-                                    </Badge>
+                                    {t('About')}
                                 </ListItemText>
                             </MenuItem>
                         </MenuList>
